1f78d3f182362496e8aedbfc6f7665fdb422d311
gasalepiji <malikggwp15@gmail.com>
2024-10-27 20:13:30 +0700

    script.js

--- view/script/script.js
+++ view/script/script.js
@@ -1,3 +1,4 @@
+// Hamburger
 function toggleMenu() {
     const menu = document.getElementById('navbar-menu');
     menu.classList.toggle('show');
@@ -24,61 +25,4 @@ document.addEventListener("DOMContentLoaded", function () {
             popup.style.display = "none";
         }
     });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Disable right-click
-    document.addEventListener('contextmenu', event => event.preventDefault());
-
-    // Dissable Shortcut
-    document.onkeydown = function(e) {
-        if (
-            e.keyCode == 123 ||  // F12
-            (e.ctrlKey && e.shiftKey && e.keyCode == 73) ||  // Ctrl+Shift+I
-            (e.ctrlKey && e.shiftKey && e.keyCode == 74) ||  // Ctrl+Shift+J
-            (e.ctrlKey && e.keyCode == 85) ||  // Ctrl+U
-            (e.ctrlKey && e.keyCode == 83) ||  // Ctrl+S
-            (e.ctrlKey && e.shiftKey && e.keyCode == 67)  // Ctrl+Shift+C
-        ) {
-            e.preventDefault();
-            alert('Devtools Terdeteksi, Harap Tutup Segera')
-            return false;
-        }
-    };
-
-    window.oncontextmenu = function(event) {
-        event.preventDefault();
-        alert('Halaman Ini Tidak Membolehkan Anda Untuk Melakukan Tindakan Klik Kanan Pada Mouse!!')
-        return false;
-    };
-
-    window.addEventListener('resize', function() {
-        if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
-            window.close();
-        }
-    });
-
-    const devtools = function() {
-        let element = new Image();
-        Object.defineProperty(element, 'id', {
-            get: function() {
-                throw 'DevTools is opened';
-            }
-        });
-
-        setInterval(function() {
-            console.profile();
-            console.profileEnd();
-            if (console.clear) {
-                console.clear();
-            }
-        }, 1000);
-    };
-
-    try {
-        devtools();
-    } catch (err) {
-        alert('DevTools is opened. Please close it!');
-        window.close();
-    }
 });
